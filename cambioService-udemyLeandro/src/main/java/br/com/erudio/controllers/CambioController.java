252cdd0e9a8d717367ec16ebed1d2b69d51967dc
ThaliaLevy <thalialevy@hotmail.com>
2023-01-26 15:59:27 -0300

    Conectando ao banco e deixando funcional a conversão de cambio

--- cambioService-udemyLeandro/src/main/java/br/com/erudio/controllers/CambioController.java
+++ cambioService-udemyLeandro/src/main/java/br/com/erudio/controllers/CambioController.java
@@ -1,24 +1,46 @@
 package br.com.erudio.controllers;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.models.Cambio;
+import br.com.erudio.repositories.CambioRepository;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
 
+	private final Environment environment;
+	private final CambioRepository cambioRepository;
+
+	public CambioController(Environment environment, CambioRepository cambioRepository) {
+		this.environment = environment;
+		this.cambioRepository = cambioRepository;
+	}
+
 	// localhost:8000/cambio-service/5/USD/BRL
 	@GetMapping(value = "/{amount}/{from}/{to}")
-	public Cambio getCambio(@PathVariable(name= "amount") BigDecimal amount,
-							@PathVariable(name= "from") String from,
-							@PathVariable(name= "to") String to) {
+	public Cambio getCambio(@PathVariable(name = "amount") BigDecimal amount, 
+							@PathVariable(name = "from") String from,
+							@PathVariable(name = "to") String to) {
+
+		var cambio = cambioRepository.findByFromAndTo(from, to);
 		
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		if(cambio == null) 
+			throw new RuntimeException("Currency unsupported");
+		
+		var port = environment.getProperty("local.server.port"); // pega a porta da aplicação
+		BigDecimal conversionFactor = cambio.getConversionFactor();
+		BigDecimal convertedValue = conversionFactor.multiply(amount);
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));	//setScale > arredonda para duas casas decimais
+		cambio.setEnvironment(port);
+
+		return cambio;
 	}
 }
--- cambioService-udemyLeandro/src/main/java/br/com/erudio/models/Cambio.java
+++ cambioService-udemyLeandro/src/main/java/br/com/erudio/models/Cambio.java
@@ -4,15 +4,35 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity(name= "cambio")
 public class Cambio implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name= "from_currency", nullable= false, length= 3)
 	private String from;
+	
+	@Column(name= "to_currency", nullable= false, length= 3)
 	private String to;
-	private String environment;
+
+	@Column(nullable= false)
 	private BigDecimal conversionFactor;
+	
+	@Transient	//usado para indicar que ele não será persistido no banco de dados
 	private BigDecimal convertedValue;
+	
+	@Transient
+	private String environment;
 
 	public Cambio() {}
 
