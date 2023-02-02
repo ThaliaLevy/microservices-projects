682314799fb5f347f19e7ad411c355af7bb51d94
ThaliaLevy <thalialevy@hotmail.com>
2023-02-02 14:33:39 -0300

    Inserindo resilience4j para tratamento de erros + uso do retry

---
+++ bookService-udemyLeandro/src/main/java/br/com/erudio/controllers/FooBarController.java
@@ -0,0 +1,26 @@
+package br.com.erudio.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+	
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);	
+
+	@GetMapping("/foo-bar")
+	//@Retry(name="default")	// caso dê erro, ele tenta mais vezes. como default tenta 3x
+	@Retry(name="foo-bar")	// caso dê erro, ele tenta mais vezes. como default tenta 3x
+	public String fooBar() {
+		logger.info("testandooooooooidfo");
+		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+		return response.getBody();
+	}
+}
