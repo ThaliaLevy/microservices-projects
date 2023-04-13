d713fbc41ec239f7e4d83bc6b3baea0086c2e130
ThaliaLevy <thalialevy@hotmail.com>
2023-04-13 11:48:45 -0300

    fix: substituindo projedto antigo por um novo

--- bookService-udemyLeandro/src/main/java/br/com/erudio/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.erudio.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.erudio.models.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
