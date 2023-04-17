805478941709e9c6be1b3dabff9056e8500a80f8
ThaliaLevy <thalialevy@hotmail.com>
2023-04-17 13:32:17 -0300

    aula 09

---
+++ msclientes/src/main/java/com/levythalia/msclientes/MsclientesApplication.java
@@ -0,0 +1,13 @@
+package com.levythalia.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientesApplication.class, args);
+	}
+
+}
