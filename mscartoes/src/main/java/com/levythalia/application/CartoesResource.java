4068ab054c8e780caeded18e2e7b1e55a5037e9c
ThaliaLevy <thalialevy@hotmail.com>
2023-04-19 17:15:35 -0300

    feat: inserindo endpoint que lista os cartoes que uma pessoa pode solicitar pela sua renda

--- mscartoes/src/main/java/com/levythalia/application/CartoesResource.java
+++ mscartoes/src/main/java/com/levythalia/application/CartoesResource.java
@@ -1,11 +1,14 @@
 package com.levythalia.application;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.levythalia.application.representation.CartaoSaveRequest;
@@ -26,9 +29,15 @@ public class CartoesResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request) {
+	public ResponseEntity<?> cadastra(@RequestBody CartaoSaveRequest request) {
 		Cartao cartao = request.toModel();
 		service.save(cartao);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@GetMapping(params = "renda")
+	public ResponseEntity<List<Cartao>> getCartoesRendaAteh(@RequestParam("renda") Long renda) {
+		List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+		return ResponseEntity.ok(list);
 	}
 }
