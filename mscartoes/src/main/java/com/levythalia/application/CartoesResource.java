b3011a6a955460c2e625b20cf7e906f8503d3dd1
ThaliaLevy <thalialevy@hotmail.com>
2023-04-18 23:09:42 -0300

    feat: novo microservice - mscartoes + configs iniciais

---
+++ mscartoes/src/main/java/com/levythalia/application/CartoesResource.java
@@ -0,0 +1,15 @@
+package com.levythalia.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cartoes")
+public class CartoesResource {
+
+	@GetMapping
+	public String status() {
+		return "ok";
+	}
+}
