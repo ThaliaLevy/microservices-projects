1f1e419e633b2fe1e925f8a4925e6385355596a2
ThaliaLevy <thalialevy@hotmail.com>
2023-04-20 16:13:16 -0300

    aula 25

--- msavaliadorcredito/src/main/java/com/levythalia/application/avaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/com/levythalia/application/avaliadorCreditoService.java
@@ -1,10 +1,14 @@
 package com.levythalia.application;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.levythalia.domain.model.CartaoCliente;
 import com.levythalia.domain.model.DadosCliente;
 import com.levythalia.domain.model.SituacaoCliente;
+import com.levythalia.infra.clients.CartoesResourceClient;
 import com.levythalia.infra.clients.ClienteResourceClient;
 
 import lombok.RequiredArgsConstructor;
@@ -14,13 +18,16 @@ import lombok.RequiredArgsConstructor;
 public class avaliadorCreditoService {
 	
 	private final ClienteResourceClient clientesClient;
+	private final CartoesResourceClient cartoesClient;
 
 	public SituacaoCliente obterSituacaoCliente(String cpf) {
-		ResponseEntity<DadosCliente> response = clientesClient.dadosCliente(cpf);
+		ResponseEntity<DadosCliente> clientesResponse = clientesClient.dadosCliente(cpf);
+		ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 		
 		return SituacaoCliente
 				.builder()
-				.cliente(response.getBody())
+				.cliente(clientesResponse.getBody())
+				.cartoes(cartoesResponse.getBody())
 				.build();
 	}
 }
---
+++ msavaliadorcredito/src/main/java/com/levythalia/infra/clients/CartoesResourceClient.java
@@ -0,0 +1,17 @@
+package com.levythalia.infra.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.levythalia.domain.model.CartaoCliente;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartoesResourceClient {
+
+	@GetMapping(params = "cpf")
+	public ResponseEntity<List<CartaoCliente>> getCartoesByCliente(@RequestParam("cpf") String cpf);
+}
